1ace41702504013962965cabd5e61c2f18fda3a4
Ornella GRISTI <ornellazaidi@gmail.com>
2019-10-07 13:43:25 +0200

    add Quotecard

---
+++ src/components/QuoteCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './QuoteCard.css';
+
+
+
+function QuoteCard (props){
+    return (<figure className="QuoteCard">
+<img src= {props.image} alt= {props.name} ></img>
+<figcaption>
+  <blockquote>{props.quote}</blockquote>
+  <cite>{props.name}</cite>
+  
+</figcaption>
+</figure>)
+}
+
+export default QuoteCard;
+
