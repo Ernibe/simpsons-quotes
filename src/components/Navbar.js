4f6a47903b7743cef979097eddaed4adef3f6f1c
Ornella GRISTI <ornellazaidi@gmail.com>
2019-10-07 10:09:48 +0200

    add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+    import React from 'react';
+    import './Navbar.css'
+
+    function Navbar (){
+        return ( <nav className= "Navbar">
+            <h1>Les Phrases Cultes Des Simpsons</h1>
+                    <a href="https://fr.wikipedia.org/wiki/Les_Simpson">lien</a>
+
+        </nav>)
+    }
+
+    export default Navbar;
