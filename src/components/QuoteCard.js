a2e7b17b6bee3a946802fa60950cca83d6f789fc
Ornella GRISTI <ornellazaidi@gmail.com>
2019-10-10 10:53:07 +0200

    add quete2

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -4,15 +4,16 @@ import './QuoteCard.css';
 
 
 function QuoteCard (props){
-    return (<figure className="QuoteCard">
-<img src= {props.image} alt= {props.name} ></img>
-<figcaption>
-  <blockquote>{props.quote}</blockquote>
-  <cite>{props.name}</cite>
-  
-</figcaption>
-</figure>)
-}
+        return (
+        <figure className="QuoteCard">
+    <img src={props.image} alt={props.name} ></img>
+    <figcaption>
+    <blockquote>{props.quote}</blockquote>
+    <cite>{props.name}</cite>
+    
+    </figcaption>
+    </figure>)
+    }
 
 export default QuoteCard;
 
